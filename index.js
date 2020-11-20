c4a6985174bf436e63f33aee594b787d64d099c2
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-11-20 20:24:16 +0800

    🎉 Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+
+const prefix = "~~";
+
+client.once("ready", () => { // prints "Ready!" to the console once the bot is online
+    console.log("Ready!");
+});
+
+client.on("message", function(message) { 
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.channel.send(`Pong! This message had a latency of ${timeTaken}ms.`, {tts: args[0]==='tts'} );
+    }
+    else if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.channel.send(`The sum of all the arguments you provided is ${sum}!`);
+    }
+    else if (command === "timetable"){
+        message.channel.send();
+    }
+
+
+});  
+
+client.login(config.BOT_TOKEN);
