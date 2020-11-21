892892c909b7838252dbaab6bbfc646254506da7
Ong Zhi Zheng <hewwo@ongzz.me>
2020-11-21 10:35:14 +0000

    patches and documentation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # botte
+
+<img src="images/botte.png" width="100px" />
+
+## prefix: `~~`
+| cmds | desc |
+|------|------|
+| `~~oi <tts>` | ping to test uptime |
+| `~~sum [numbers]` | sum together the numbers u give it |
+| `~~count *number* <tts>` | counts from 0 to your number with tts option |
+| `~~rap <number>` | raps tss song, with length option |
+| `~~ttb *class*` | sends image of class timetable |
+| `~~link set *class* *period* *link*` | sets a link for a class |
+| `~~link delete *class* *period*` | deletes a link for a class |
+| `~~link *class* *period*` | gets a link for a class |
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.listen(port, () => console.log(`listening at http://localhost:${port}`));
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const Database = require("@replit/database");
+const db = new Database();
+
 
 const prefix = "~~";
 const timetableUrl = {
@@ -16,6 +19,12 @@ const timetableUrl = {
 	"F4M": "https://raw.githubusercontent.com/Fogeinator/botte/main/images/ttbs/f4m.png",
 	"S1SH": "https://raw.githubusercontent.com/Fogeinator/botte/main/images/ttbs/s1sh.png",
 };
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -31,70 +40,112 @@ client.on("message", function(message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-
-    if (command === "oi") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.channel.send(
-			`u oi me for what... i reply in ${timeTaken}ms.`, 
-			{tts: args[args.length-1]==='tts'} 
-		);
-    }
-    else if (command === "sum") {
-        const numArgs = args.map(x => parseFloat(x));
-        const sum = numArgs.reduce((counter, x) => counter += x);
-        message.channel.send(
-			`The sum of all the arguments you provided is ${sum}!`
-		);
-    }
-    else if (command === "count") {
-        let x = 0;
-        while(x <= args[0]){
-        	message.channel.send(
-				x.toString(), 
-				{tts: args[args.length-1]==='tts'}
+	try{
+		if (command === "oi") {
+			//~~oi <tts>
+			const timeTaken = Date.now() - message.createdTimestamp;
+			message.channel.send(
+				`u oi me for what... i reply in ${timeTaken}ms.`, 
+				{tts: args[args.length-1]==='tts'} 
 			);
-        	sleep(1000);
-			x += 1;
-        }
-    }
-	else if (command === "rap") {
-		let rap = "tsstsststsstssts ";
-		let ttsString = ""; 
-        if(!args[0]){
-			ttsString = rap
-		} else {
-			let x = 1;
+		}
+		else if (command === "sum") {
+			//~~sum [loads of numbers]
+			const numArgs = args.map(x => parseFloat(x));
+			const sum = numArgs.reduce((counter, x) => counter += x);
+			isNaN(sum) ?
+			message.channel.send(`you can only sum numbers dumbass`)
+			:
+			message.channel.send(
+				`The sum of all the arguments you provided is ${sum}!`
+			);
+		}
+		else if (command === "count") {
+			//~~count *num* <tts>
+			let x = 0;
 			while(x <= args[0]){
-				ttsString += rap;
+				message.channel.send(
+					x.toString(), 
+					{tts: args[args.length-1]==='tts'}
+				);
+				sleep(1000);
 				x += 1;
 			}
 		}
-		message.channel.send(ttsString, {tts: true});
+		else if (command === "rap") {
+			//~~rap <69>
+			let rap = "tsstsststsstssts ";
+			let ttsString = ""; 
+			if(!args[0]){
+				ttsString = rap
+			} else {
+				let x = 1;
+				while(x <= args[0]){
+					ttsString += rap;
+					x += 1;
+				}
+			}
+			message.channel.send(ttsString, {tts: true});
+		}
+		else if (command === "ttb"){
+			//~~ttb f4m
+			let ttbUrl = timetableUrl[args[0].toUpperCase()];
+			ttbUrl ? 
+			message.channel.send(`${args[0].toUpperCase()} timetable`, 
+			{files: [ ttbUrl ]})
+			: 
+			message.channel.send("send what cacat stuff i cant find eh", 
+			{files: [ timetableUrl["err"] ]})
+		}
+		else if (command === "link"){
+			if(args[0] === "set"){
+				//~~link set f4m bio eaubaougboaegoaeoigbea
+				let classss = args[1].toUpperCase();
+				let period = args[2].toUpperCase();
+				let zoomLink = args[3];
+				
+				(classss && period && zoomLink) ?
+				db.set(`${classss}_${period}`, `${zoomLink}`)
+				.then(() => message.channel.send(
+					`I saved ${zoomLink} to ${classss}_${period}`
+				))
+				:
+				message.channel.send(`one of your params are missing, check again...`);
+			} 
+			else if (args[0] === "delete"){
+				//~~link delete f4m bio
+				let classss = args[1].toUpperCase();
+				let period = args[2].toUpperCase();
+
+				(classss && period) ?
+				db.delete(`${classss}_${period}`)
+				.then(() => message.channel.send(
+					`I just deleted ${classss}'s ${period} link... I hope you know what you're doing kiddo!`
+				))
+				:
+				message.channel.send(`one of your params are missing, check again...`);
+			}
+			else {
+				//~~link f4m bio
+				let classss = args[0].toUpperCase();
+				let period = args[1].toUpperCase();
+				db.get(`${classss}_${period}`).then(value => {
+					value ? 
+					message.channel.send(
+						`${classss} ${period} class link: ${value}`
+					)
+					: 
+					message.channel.send(
+						`i think u not yet run: ~~link set ${args[0]} ${args[1]} <link>` 
+					)
+				});
+			}
+		}
+	} catch (err) {
+		console.error(err);
+		message.channel.send("im sry u say what idk pun")
 	}
-	else if (command === "ttb"){
-		let ttbUrl = timetableUrl[args[0].toUpperCase()];
-        ttbUrl ? 
-		message.channel.send(`${args[0].toUpperCase()} timetable`, 
-		{files: [ ttbUrl ]})
-		: 
-		message.channel.send("send what cacat stuff i cant find eh", 
-		{files: [ timetableUrl["err"] ]})
-    }
-	// else if (){
-
-	// }
-
 
 });  
 
 client.login(process.env.BOT_TOKEN);
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-function getCurrentPeriod(){
-	
-}
